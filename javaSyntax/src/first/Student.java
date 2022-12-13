bd2edb37aecb09092c0aca42c6fa9c9a5771be78
Taemin Lee <dlrgks88@naver.com>
2022-12-14 01:14:22 +0900

    Constructor-1
    - default constructor

--- javaSyntax/src/first/Student.java
+++ javaSyntax/src/first/Student.java
@@ -8,6 +8,20 @@ public class Student {
 
     String address;
 
+//    public Student() {
+//    }
+
+
+    public Student(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Student(int id, String name){
+        this.studentId = id;
+        this.studentName = name;
+        this.address = "주소 없음";
+    }
+
     public void showStudentInfo(){
         System.out.println("studentName = " + studentName);
         System.out.println("address = " + address);
--- javaSyntax/src/first/StudentTest.java
+++ javaSyntax/src/first/StudentTest.java
@@ -2,10 +2,16 @@ package first;
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student studentLee = new Student();
+        Student studentLee = new Student("이태민");
         studentLee.studentName = "이태민";
         studentLee.address = "서울";
 
         studentLee.showStudentInfo();
+
+        Student studentKim = new Student(1,"김유신");
+        studentKim.studentName = "김유신";
+//        studentKim.address = "경주";
+
+        studentKim.showStudentInfo();
     }
 }

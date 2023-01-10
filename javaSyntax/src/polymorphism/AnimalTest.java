f75ed7bae8d53bfda7fdff094d3967c639e50c2d
Taemin Lee <dlrgks88@naver.com>
2023-01-10 16:16:33 +0900

    다운캐스팅과 instanceof

--- javaSyntax/src/polymorphism/AnimalTest.java
+++ javaSyntax/src/polymorphism/AnimalTest.java
@@ -13,6 +13,10 @@ class Human extends Animal {
     public void move() {
         System.out.println("사람이 두발로 걷습니다.");
     }
+
+    public void read() {
+        System.out.println("사람이 책을 읽습니다.");
+    }
 }
 
 class Tiger extends Animal {
@@ -20,12 +24,20 @@ class Tiger extends Animal {
     public void move() {
         System.out.println("호랑이가 네 발로 뜁니다.");
     }
+
+    public void hunting() {
+        System.out.println("호랑이가 사냥을 합니다.");
+    }
 }
 
 class Eagle extends Animal {
     @Override
     public void move() {
         System.out.println("독수리가 하늘을 날아갑니다.");
+    }
+
+    public void flying() {
+        System.out.println("독수리가 날개를 쭉 펴고 날아갑니다.");
     }
 }
 
@@ -36,19 +48,50 @@ public class AnimalTest {
         Animal tAnimal = new Tiger();
         Animal eAnimal = new Eagle();
 
-        AnimalTest test = new AnimalTest();
-        test.moveAnimal(hAnimal);
-        test.moveAnimal(tAnimal);
-        test.moveAnimal(eAnimal);
+
+        //instanceof로 체크
+//        if (hAnimal instanceof Human) {
+//            Human human = (Human) hAnimal;
+//            human.read();
+//        }
+
+
+        //다운 캐스팅
+//        Human human = (Human) hAnimal;
+//        human.read();
+//        AnimalTest test = new AnimalTest();
+//        test.moveAnimal(hAnimal);
+//        test.moveAnimal(tAnimal);
+//        test.moveAnimal(eAnimal);
 
         ArrayList<Animal> animalList = new ArrayList<Animal>();
         animalList.add(hAnimal);
         animalList.add(eAnimal);
         animalList.add(tAnimal);
 
+        AnimalTest test = new AnimalTest();
+        test.testDownCasting(animalList);
 
-        for (Animal animal : animalList) {
-            animal.move();
+//        for (Animal animal : animalList) {
+//            animal.move();
+//        }
+    }
+    // 안정적으로 다운캐스팅을 하기위해서는 instanceof를 쓴다.
+    public void testDownCasting(ArrayList<Animal> list) {
+        for (int i = 0; i < list.size(); i++) {
+            Animal animal = list.get(i);
+            if (animal instanceof Human) {
+                Human human = (Human) animal;
+                human.read();
+            }
+            else if (animal instanceof Tiger) {
+                Tiger tiger = (Tiger) animal;
+                tiger.hunting();
+            }
+            else if (animal instanceof Eagle) {
+                Eagle eagle = (Eagle) animal;
+                eagle.flying();
+            }
         }
     }
 

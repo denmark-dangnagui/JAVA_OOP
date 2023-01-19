36c7043e7332bb9af7fc5be0eedb473e73a7aa65
Taemin Lee <dlrgks88@naver.com>
2023-01-19 23:48:54 +0900

    Update LeastJob.java

--- javaSyntax/src/scheduler/LeastJob.java
+++ javaSyntax/src/scheduler/LeastJob.java
@@ -8,6 +8,6 @@ public class LeastJob implements Scheduler {
 
     @Override
     public void sendCallAgent() {
-        System.out.println("현재 상담업무가 없거나 상담대기가 가장 적은 상담원에게 할당합니다.");
+        System.out.println(" 현재 상담업무가 없거나 상담대기가 가장 적은 상담원에게 할당합니다.");
     }
 }
